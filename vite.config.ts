6de6c49f31ecf4c1623f8e21673dea7807675118
IRONDESK <zerozerobase@gmail.com>
2025-08-19 23:35:33 +0900

    - svgr, import paths setting

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
+import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), vanillaExtractPlugin()],
+  plugins: [react(), vanillaExtractPlugin(), svgr()],
   resolve: {
     alias: [{ find: '@', replacement: '/src' }],
   },
