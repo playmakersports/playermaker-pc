61f4ca74f5f90d4a43a14c37e621b44160449116
IRONDESK <zerozerobase@gmail.com>
2025-09-07 00:01:32 +0900

    - 홈, 관리 메인 페이지

---
+++ src/pages/admin/AdminIndex.tsx
@@ -0,0 +1,5 @@
+function AdminLayout() {
+  return <>메뉴에서 선택해 주세요</>;
+}
+
+export default AdminLayout;
--- src/pages/home/HomeIndex.tsx
+++ src/pages/home/HomeIndex.tsx
@@ -1,5 +1,19 @@
+import Button from '@/share/components/Button.tsx';
+
 function HomeIndex() {
-  return <>Test</>;
+  return (
+    <main>
+      <div></div>
+      <div>
+        <Button type="button" mode="gray" fillType="outline" size="xlarge">
+          구글 로그인
+        </Button>
+        <Button type="button" mode="gray" fillType="outline" size="xlarge">
+          카카오 로그인
+        </Button>
+      </div>
+    </main>
+  );
 }
 
 export default HomeIndex;
