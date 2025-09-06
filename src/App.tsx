cd73a1c609ece27ad9be9afd681fb26f7b742ea8
IRONDESK <zerozerobase@gmail.com>
2025-09-06 23:20:55 +0900

    - Query Client 설정 수정, 전역 에러 처리

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,35 @@ import './style/reset.css';
 import './style/pretendardvariable.css';
 import '@/style/theme.css.ts';
 import Router from './routes';
-import { Provider } from 'jotai';
+import { Provider as JotaiProvider } from 'jotai';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { overlay } from 'overlay-kit';
+import { Dialog } from '@/share/common/Dialog.tsx';
 
 function App() {
-  const queryClient = new QueryClient();
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+        retry: (failureCount, error) => {
+          if (failureCount === 1) {
+            overlay.open(controller => (
+              <Dialog title="서버 통신 오류" contents={error.message} type="alert" {...controller} />
+            ));
+            return false;
+          }
+          return true;
+        },
+      },
+    },
+  });
 
   return (
-    <>
-      <Provider>
-        <QueryClientProvider client={queryClient}>
-          <Router />
-        </QueryClientProvider>
-      </Provider>
-    </>
+    <JotaiProvider>
+      <QueryClientProvider client={queryClient}>
+        <Router />
+      </QueryClientProvider>
+    </JotaiProvider>
   );
 }
 
